9db0c4ccdc1f3694fc23f6b5d752fd3abd323338
triwaluyo07 <triwaluyo432@gmail.com>
2020-12-31 18:48:31 +0700

    RecycleViewPahlawan

---
+++ MyRecyclerView/app/src/main/java/triwaluyo/dicoding/myrecyclerview/ListHeroAdapter.java
@@ -0,0 +1,83 @@
+package triwaluyo.dicoding.myrecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>
+{
+    private ArrayList<Hero> listHero;
+    private OnItemClickCallback onItemClickCallback;
+
+    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    public interface OnItemClickCallback
+    {
+        void onItemClicked(Hero data);
+    }
+
+    public ListHeroAdapter(ArrayList<Hero> listHero) {
+        this.listHero = listHero;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_new_hero,parent,false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ListViewHolder holder, int position) {
+        Hero hero = listHero.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55,55))
+                .into(holder.imgPhoto);
+        holder.tvName.setText(hero.getName());
+        holder.tvDetail.setText(hero.getDetail());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickCallback.onItemClicked(listHero.get(holder.getAdapterPosition()));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHero.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgPhoto;
+        TextView tvName,tvDetail;
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvDetail = itemView.findViewById(R.id.tv_item_detail);
+        }
+    }
+
+
+
+}
